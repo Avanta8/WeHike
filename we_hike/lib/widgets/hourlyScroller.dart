dcab3fdf8e30229f114d3c5706ae5277dffd7e58
aaron <aswf2@cam.ac.uk>
2023-05-18 15:53:35 +0100

    updated scroller

--- we_hike/lib/widgets/hourlyScroller.dart
+++ we_hike/lib/widgets/hourlyScroller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:we_hike/widgets/current_weather_icon.dart';
 import 'package:we_hike/widgets/white_text.dart';
 
 class HourlyScroller extends StatelessWidget{
@@ -9,27 +10,45 @@ class HourlyScroller extends StatelessWidget{
   Widget build(BuildContext context) {
     var now = DateTime.now();
     return ListView(
-      scrollDirection: Axis.horizontal,
-      children: [
-        for (int time = 0; time <= 23;time++)
-          Padding(
-            padding: const EdgeInsets.all(3.0),
-            child: Card(
-              color: Colors.grey,
-
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child:
-                  Column(
-                    children: [
-                      WhiteText(text: DateFormat('HH:mm').format(DateTime(now.year,now.month,now.day,time)),),
-                      //TODO insert weather data
-                    ],
+        scrollDirection: Axis.horizontal,
+        children: [
+          for (int time = 0; time <= 23;time++)
+            Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Card(
+                color: Colors.transparent,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width / 4.5,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.5),
+                        borderRadius: BorderRadius.only(
+                          topLeft:Radius.circular(20),
+                          topRight:Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                          bottomRight:Radius.circular(20),
+                        )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child:
+                      Column(
+                        children: [
+                          SizedBox(height: 15,),
+                          WhiteText(text: DateFormat('HH:mm').format(DateTime(now.year,now.month,now.day,time)),),
+                          SizedBox(height: 10,),
+                          //TODO API Icon code
+                          Center(child: CurrentWeatherIcon(iconCode: 113)),
+                          SizedBox(height: 20,),
+                          WhiteText(text: "15°", size: 25),
+                        ],
+                      ),
+                    ),
                   ),
+                ),
               ),
             ),
-          ),
-      ]
+        ]
     );
   }
 }
