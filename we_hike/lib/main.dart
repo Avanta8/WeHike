d09bc1d2312f9b59c7afa79d22524c4e4195ba21
thejadeassassin <ironmanmark3072004@gmail.com>
2023-05-18 15:18:22 +0100

    undid a dummy function

--- we_hike/lib/main.dart
+++ we_hike/lib/main.dart
@@ -3,12 +3,6 @@ import 'package:we_hike/homelayout/main.dart';
 import 'package:we_hike/my_api/api_calls.dart';
 import 'package:flutter/material.dart';
 
-
-String _echo(String userQuery) {
-
-    return userQuery;
-  }
-
 void main() {
   runApp(HomePage());
 }
