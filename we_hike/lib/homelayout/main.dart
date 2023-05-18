85dcb6adbd4c75ce9f8c9a6a4098b57b193ea709
Lucy Robertson <lucy.robertson135@gmail.com>
2023-05-18 16:21:13 +0100

    got string from future string

--- we_hike/lib/homelayout/main.dart
+++ we_hike/lib/homelayout/main.dart
@@ -114,179 +114,185 @@ class _LayoutState extends State<Layout> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/hills.jpg'),
-            fit: BoxFit.fitHeight,
-            )
-        ),
-        child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                            child: GestureDetector(
-                              child: SizedBox(
-                                      height: 35,
-                                      child: svg,
-                                    ),
-                              onTap: () {
-                                        // move to search screen
-                                        Navigator.push(
-                                          context, 
-                                          MaterialPageRoute(builder: (context) => const SearchPage()));
-                                      },
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 20, 15, 5),
-                              child: Column(
-                                children: const [
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: ClockWidget()
-                                    ),
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: WhiteText(text: "Current Location:"),
-                                    ),
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: WhiteText(text: "Dartmoor",
-                                    )
-                                    ),
-                                ],
-                              ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(20, 0, 0, 5),
-                              child: Column(
-                                children: const [
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: WhiteText(
-                                      text: "15°",
-                                      size: 30
-                                    ),
-                                  ),
-                                  Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: WhiteText(text: "Rainy", size: 25)
-                                  )
-                                ],
-                            ),
-                          ),
-                        ),
-                        const CurrentWeatherIcon(iconCode: 113,),
-                      ],
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        height: 200,
-                        child: Expanded(child: HourlyScroller(),)
-                      ),
-                    ),
-
-                    const SunTimes(sunriseTime: "06:07", sunsetTime: "21:42"),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        color: Colors.amber,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: const [
-                              Expanded(
-                                flex: 5,
-                                child: Text("Wind speed")
-                                ),
-                              Expanded(
-                                flex: 3,
-                                child: Text("2 km/h")
-                                ),
-                              Expanded(
-                                flex: 1,
-                                child: Center(child: Text("v"))
-                                )
-                          ]),
-                        ),
-                      ),
-                    ),
-      
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        color: Colors.amber,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: const [
-                              Expanded(
-                                flex: 5,
-                                child: Text("Cloud Coverage")
-                                ),
-                              Expanded(
-                                flex: 3,
-                                child: Text("12%")
-                                ),
-                              Expanded(
-                                flex: 1,
-                                child: Center(child: Text("v"))
-                                )
-                          ]),
-                        ),
-                      ),
-                    ),
-      
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        color: Colors.amber,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: const [
-                              Expanded(
-                                flex: 5,
-                                child: Text("Chance of Rainfall")
-                                ),
-                              Expanded(
-                                flex: 3,
-                                child: Text("25%")
-                                ),
-                              Expanded(
-                                flex: 1,
-                                child: Center(
-                                  child: Text("v"))
-                                )
-                          ]),
-                        ),
-                      ),
-                    ),
-
-                    Padding(padding: const EdgeInsets.all(8),
-                    child: _bottomBar(),)
-                ],
-              )
+    return FutureBuilder(
+      future: widget._getLocationFromMemory(),
+      builder: (context, snapshot) {
+        String location = snapshot.data!;
+        return Scaffold(
+          body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/hills.jpg'),
+                fit: BoxFit.fitHeight,
+                )
             ),
-      ),
-      );
+            child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                                child: GestureDetector(
+                                  child: SizedBox(
+                                          height: 35,
+                                          child: svg,
+                                        ),
+                                  onTap: () {
+                                            // move to search screen
+                                            Navigator.push(
+                                              context, 
+                                              MaterialPageRoute(builder: (context) => const SearchPage()));
+                                          },
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 20, 15, 5),
+                                  child: Column(
+                                    children: [
+                                      const Align(
+                                        alignment: Alignment.centerRight,
+                                        child: ClockWidget()
+                                        ),
+                                      const Align(
+                                        alignment: Alignment.centerRight,
+                                        child: WhiteText(text: "Current Location:"),
+                                        ),
+                                      Align(
+                                        alignment: Alignment.centerRight,
+                                        child: WhiteText(text: location,
+                                        )
+                                        ),
+                                    ],
+                                  ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(20, 0, 0, 5),
+                                  child: Column(
+                                    children: const [
+                                      Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: WhiteText(
+                                          text: "15°",
+                                          size: 30
+                                        ),
+                                      ),
+                                      Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: WhiteText(text: "Rainy", size: 25)
+                                      )
+                                    ],
+                                ),
+                              ),
+                            ),
+                            const CurrentWeatherIcon(iconCode: 113,),
+                          ],
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            height: 200,
+                            child: Expanded(child: HourlyScroller(),)
+                          ),
+                        ),
+
+                        const SunTimes(sunriseTime: "06:07", sunsetTime: "21:42"),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: Colors.amber,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: const [
+                                  Expanded(
+                                    flex: 5,
+                                    child: Text("Wind speed")
+                                    ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Text("2 km/h")
+                                    ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Center(child: Text("v"))
+                                    )
+                              ]),
+                            ),
+                          ),
+                        ),
+          
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: Colors.amber,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: const [
+                                  Expanded(
+                                    flex: 5,
+                                    child: Text("Cloud Coverage")
+                                    ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Text("12%")
+                                    ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Center(child: Text("v"))
+                                    )
+                              ]),
+                            ),
+                          ),
+                        ),
+          
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: Colors.amber,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: const [
+                                  Expanded(
+                                    flex: 5,
+                                    child: Text("Chance of Rainfall")
+                                    ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Text("25%")
+                                    ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Center(
+                                      child: Text("v"))
+                                    )
+                              ]),
+                            ),
+                          ),
+                        ),
+
+                        Padding(padding: const EdgeInsets.all(8),
+                        child: _bottomBar(),)
+                    ],
+                  )
+                ),
+          ),
+          );
+      }
+    );
   }
 }
