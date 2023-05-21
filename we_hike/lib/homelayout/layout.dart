abb03b105277219b4e1a0902788b8d9b8b7d7f24
Harry Min <harryhmin@gmail.com>
2023-05-21 16:20:09 +0100

    safebar for error page

--- we_hike/lib/homelayout/layout.dart
+++ we_hike/lib/homelayout/layout.dart
@@ -31,75 +31,72 @@ class Layout extends StatefulWidget {
   // this method checks if a weather model has been passed into Layout, and if not returns the weather in Edinburgh
   // the app therefore always checks the weather in Edinburgh when it is opened
   Future<futureModel?>? _getWeatherModel() {
-    if(weatherForecast is Future<futureModel?>) {
+    if (weatherForecast is Future<futureModel?>) {
       return weatherForecast;
-    }
-    else {
+    } else {
       return getForecastFromCurrentLocation();
     }
   }
 
   final Widget svg = SvgPicture.asset(
-  'assets/magnifying-glass-solid.svg',
-  colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-  semanticsLabel: 'Magnifying glass',
-  alignment: Alignment.centerLeft,
+    'assets/magnifying-glass-solid.svg',
+    colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+    semanticsLabel: 'Magnifying glass',
+    alignment: Alignment.centerLeft,
   );
 }
 
 class _LayoutState extends State<Layout> {
-
   // the build method puts the widget tree onto the screen
   @override
   Widget build(BuildContext context) {
-
     // FutureBuilder gets the weather model and returns the weather
     // forecast home screen if there is a valid weather model
     return FutureBuilder(
-      future: widget._getWeatherModel(),
-      builder: (context, snapshot) {
-        if(snapshot.hasData) {
-        futureModel currentWeatherModel = snapshot.data!;
-        return HomeScreen(currentWeatherModel: currentWeatherModel);
-      }
+        future: widget._getWeatherModel(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            futureModel currentWeatherModel = snapshot.data!;
+            return HomeScreen(currentWeatherModel: currentWeatherModel);
+          }
 
-      // if the weather model is null, the background image of the weather app
-      // is shown until the weather model is returned
-      else{
-        return Scaffold(
-          body: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
+          // if the weather model is null, the background image of the weather app
+          // is shown until the weather model is returned
+          else {
+            return Scaffold(
+                body: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
                 image: AssetImage('assets/hills.jpg'),
                 fit: BoxFit.fitHeight,
-                )
-            ),
-          child: GestureDetector(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
-              // the GestureDetector allows the magnifying glass to direct us to the search page when pressed
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: widget.svg,
+              )),
+              child: SafeArea(
+                child: GestureDetector(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+                    // the GestureDetector allows the magnifying glass to direct us to the search page when pressed
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: SizedBox(
+                        height: 50,
+                        width: 50,
+                        child: widget.svg,
+                      ),
+                    ),
+                    // move to search screen
                   ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SearchPage()));
+                  },
                 ),
-                // move to search screen
               ),
-              onTap: () {
-                Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (context) => const SearchPage()));
-              },
-            ),
-          )
-        );
-      }
-      }
-    );
+            ));
+          }
+        });
   }
 }
