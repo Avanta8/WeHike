62c641d1012ec81bcdf5bec730dcf3638383f62e
thejadeassassin <ironmanmark3072004@gmail.com>
2023-05-21 15:14:09 +0100

    further

--- we_hike/lib/my_api/api_calls.dart
+++ we_hike/lib/my_api/api_calls.dart
@@ -88,7 +88,7 @@ Future<Position> _determinePosition() async {
 
 
 
-void requestPermission() async {
+Future<String> requestPermission() async {
   bool serviceEnabled;
   LocationPermission permission;
   // Test if location services are enabled.
@@ -96,7 +96,8 @@ void requestPermission() async {
   permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
-  }
+  }\
+  return "OPK";
 }
 
 Future<Position> getLocation() async {
